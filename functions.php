62d76e45dc5a7e98c3485a79c417122cb2a9825d
Raymond Kao <ray@peopleandcode.com>
2013-02-26 00:48:39 -0500

    updated the publish action

--- functions.php
+++ functions.php
@@ -324,36 +324,37 @@ function pc_3dscan_sortable_columns( $columns ) {
 }
 
 
-class emailer {
+class pc_scan_emailer {
   function send($post_ID)  {
-    $friends = 'raykao@gmail.com';
-    wp_mail($friends,"sally's blog updated $post_ID",'I just put something on my blog: http://blog.example.com');
+		$args = array(
+			'post_type' => 'attachment',
+			'numberposts' => -1,
+			'post_status' => null,
+			'post_mime_type' => 'application/zip',
+			'post_parent' => get_the_ID()
+		); 
+
+		$attachments = get_posts($args);
+		if ($attachments) :
+			setup_postdata($attachments[0]); 
+			$link = wp_get_attachment_url($post->ID);
+		endif;
+
+  	$blog_url = bloginfo('url');
+  	$email = get_post_meta($post_ID, 'pc_3dscan_email', true);
+  	$name = ucwords(get_post_meta($post_ID, 'pc_3dscan_fname', true));
+  	$link = ;
+    $to = &$email;
+		$headers = 'From: My 3D Scan <info@my3dscan.ca>' . "\r\n" . 'Reply-To: info@my3dscan.ca';
+		$subject = "$name, your 3D Scan is now ready!"
+		$body = "Hey $name,\n\nYour 3D Scan is now ready.  You can download it here: $link \n\n\nOR\n\n\nGo to $blog_url/download and enter your email or twitter handle (if you provided us with it).\n\nPEACE!\n\n@My3DScan\n\n@DraftPrint3D\n\n@PeopleAndCode";
+
+    wp_mail($to, $subject, $body, $headers );
     return $post_ID;
   }
 }
-$myEmailClass = new emailer();
+
+$myEmailClass = new pc_scan_emailer();
 add_action('publish_pc_3dscan', array($myEmailClass, 'send'));
-
-// add_action('publish_post', 'pc_3dscan_publish');
-
-// add_filter('publish_post', 'pc_3dscan_publish');
-
-// add_action('publish_pc_3dscan', 'pc_3dscan_publish');
-
-// function pc_3dscan_publish($post_id) {
-// 	if(get_post_type($post_id) == 'pc_3dscan'){
-// 		$email = "raykao@gmail.com";
-// 		$subject = "test";
-// 		$body = "Test.";
-// 		$headers = 'From: My 3D Scan <info@my3dscan.ca>' . "\r\n" . 'Reply-To: info@my3dscan.ca';
-// 		if(wp_mail($email, $subject, $body, $headers)){
-// 			echo("<p>Message successfully sent!</p>");
-// 		} else {
-// 			echo("<p>Message delivery failed...</p>");
-// 		}
-// 	} else {
-// 		echo("<p>post type error...</p>");
-// 	}
-// }
 
 ?>
